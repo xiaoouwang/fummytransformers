4eb476d3009b5a1c7be8190a70135f194372f683
xiaoou <16024291+xiaoouwang@users.noreply.github.com>
2022-10-13 16:37:18 +0200

    Regular update

---
+++ change_version.py
@@ -0,0 +1,17 @@
+from fummytransformers import files
+import re
+import sys
+
+text = files.read_file(sys.argv[1])
+
+# print(text)
+
+new_text = re.sub(r"(0.0.)(\d+)", lambda x: str(x.group(1)) +
+                  str(int(x.group(2))+1), text)
+
+files.write_file(sys.argv[1], new_text)
+print("from")
+print(re.findall(r"0.0.\d+", new_text)[0])
+print("to")
+print(re.findall(r"0.0.\d+", new_text)[0])
+# print(new_text)
--- fummytransformers/utils.py
+++ fummytransformers/utils.py
@@ -4,8 +4,21 @@ import sys
 from importlib import reload
 
 
+def show_gpuinfo():
+    import tensorflow as tf
+    print("gpu list:", tf.config.list_physical_devices('GPU'))
+    print("n of GPUs Available: ", len(
+        tf.config.experimental.list_physical_devices('GPU')))
+    sys_details = tf.sysconfig.get_build_info()
+    try:
+        cuda_version = sys_details["cuda_version"]
+        print("cuda_version:", cuda_version)
+    except:
+        print("no cuda")
+
+
 def say_hello():
-    print("hello this is frenchnlp!")
+    print("hello this is fummytransformers!")
 
 
 def timer(func):
